c87bef9b9b84ae3430d7101c0997d3ddda258f91
aykutkucukler <79278945+aykutkucukler@users.noreply.github.com>
2022-06-16 09:29:51 +0300

    page

--- lib/screens/forgot_password.dart
+++ lib/screens/forgot_password.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gro_app_ui/screens/login_screen.dart';
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({Key? key}) : super(key: key);
@@ -10,6 +11,71 @@ class ForgotPassword extends StatefulWidget {
 class _ForgotPasswordState extends State<ForgotPassword> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+
+          children: [
+            Center(child: Image.asset("assets/images/forgot.png")),
+            SizedBox(height: 75,
+
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text("Forgot Password",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text("Enter your email and password",
+                  style: TextStyle(fontWeight: FontWeight.normal, fontSize: 13),
+                ),
+              ],
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'E-mail',
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: ( ) {
+
+            }, child: Text ("Send E-mail Create New Password")
+            ),
+            Container(
+              alignment: FractionalOffset.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {  },
+                    child: Text('Back to', style: TextStyle(color: Color(0xFF2E3233))),
+                  ),
+                  TextButton(
+                    child: Text('Log In', style: TextStyle(color: Color(0xFF84A2AF), fontWeight: FontWeight.bold),),
+                    onPressed: (){
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const LoginPage()
+                          )
+                      );
+
+                    },
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
