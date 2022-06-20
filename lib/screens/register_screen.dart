e3e876c5d8745a133c6ff9cd9b8aaaa2f4953d07
aykutkucukler <79278945+aykutkucukler@users.noreply.github.com>
2022-06-20 08:57:25 +0300

    new

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -13,120 +13,118 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body:Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 30),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(child: Image.asset('assets/images/loging.png'),
-                  ),
+    return Scaffold(
+      body:Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(child: Image.asset('assets/images/loging.png'),
                 ),
-                const SizedBox(height: 30),
+              ),
+              const SizedBox(height: 30),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Text('Sign up',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Text('Sign up',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Text('Enter your credentials to continue',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontWeight: FontWeight.normal, fontSize: 18),
+                      ),
+                ],
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: "Username.",
+                ),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: "E-mail",
+                ),
+              ),
+              TextField(
+                  obscureText: obsecure,
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(icon:const Icon(Icons.remove_red_eye),
+                      onPressed: () {setState((){
+                        obsecure =! obsecure;
+                      }); },
                     ),
+                    border: const UnderlineInputBorder(),
+                    labelText: "Password",
+                  )),
+              Container(
+                alignment: FractionalOffset.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('By continuing agree to our', style: TextStyle(color: Color(0xFF2E3233))),
+                    ),
+                    TextButton(
+                      onPressed: () {  },
+                      child: const Text('Term of services and services', style: TextStyle(color: Color(0xFF84A2AF), fontWeight: FontWeight.bold),),
+
+
+
+
+                    )
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Text('Enter your credentials to continue',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontWeight: FontWeight.normal, fontSize: 18),
-                        ),
+              ),
+
+              SizedBox( height: 50, width: 360,
+                child: ElevatedButton(
+                  onPressed: () {
+
+                  }, child: const Text('Sign In'),
+
+
+                ),
+              ),
+
+              Container(
+                alignment: FractionalOffset.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: () {  },
+                      child: const Text('Already have an account?', style: TextStyle(color: Color(0xFF2E3233))),
+                    ),
+                    TextButton(
+                      child: const Text('Log In', style: TextStyle(color: Color(0xFF84A2AF), fontWeight: FontWeight.bold),),
+                      onPressed: (){
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const LoginPage()
+                            )
+                        );
+
+                      },
+                    )
                   ],
                 ),
-                const TextField(
-                  decoration: InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: "Username.",
-                  ),
-                ),
-                const TextField(
-                  decoration: InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: "E-mail",
-                  ),
-                ),
-                TextField(
-                    obscureText: obsecure,
-                    decoration: InputDecoration(
-                      suffixIcon: IconButton(icon:const Icon(Icons.remove_red_eye),
-                        onPressed: () {setState((){
-                          obsecure =! obsecure;
-                        }); },
-                      ),
-                      border: const UnderlineInputBorder(),
-                      labelText: "Password",
-                    )),
-                Container(
-                  alignment: FractionalOffset.center,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text('By continuing agree to our', style: TextStyle(color: Color(0xFF2E3233))),
-                      ),
-                      TextButton(
-                        onPressed: () {  },
-                        child: const Text('Term of services and services', style: TextStyle(color: Color(0xFF84A2AF), fontWeight: FontWeight.bold),),
-
-
-
-
-                      )
-                    ],
-                  ),
-                ),
-
-                SizedBox( height: 50, width: 360,
-                  child: ElevatedButton(
-                    onPressed: () {
-
-                    }, child: const Text('Sign In'),
-
-
-                  ),
-                ),
-
-                Container(
-                  alignment: FractionalOffset.center,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TextButton(
-                        onPressed: () {  },
-                        child: const Text('Already have an account?', style: TextStyle(color: Color(0xFF2E3233))),
-                      ),
-                      TextButton(
-                        child: const Text('Log In', style: TextStyle(color: Color(0xFF84A2AF), fontWeight: FontWeight.bold),),
-                        onPressed: (){
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const LoginPage()
-                              )
-                          );
-
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
