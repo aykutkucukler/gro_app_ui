9e348384c4a9cf69dcc29b6cb96ae01ff98d6365
aykutkucukler <79278945+aykutkucukler@users.noreply.github.com>
2022-06-29 12:51:33 +0300

    account screen

--- lib/screens/home_screen_account.dart
+++ lib/screens/home_screen_account.dart
@@ -11,8 +11,61 @@ class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Account"),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: const [
+                CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/profilepic.png"),
+                  maxRadius: 50,
+                  minRadius: 50,
+                ),
+                Text(
+                  "Ayşe Can Ercan\n",
+                  style: TextStyle(fontSize: 20, color: Colors.black54),
+                ),
+                Text(
+                  "aysecan@gmail.com",
+                  style: TextStyle(fontSize: 20, color: Colors.black54),
+                ),
+              ],
+            ),
+          ),
+          Row(children: const <Widget>[
+            Icon(Icons.shopping_bag_outlined),
+            Text("Orders")
+          ]),
+          Row(children: const <Widget>[
+            Icon(Icons.account_circle_outlined),
+            Text("My Details")
+          ]),
+          Row(children: const <Widget>[
+            Icon(Icons.location_on_outlined),
+            Text("Delivery Address")
+          ]),
+          Row(children: const <Widget>[
+            Icon(Icons.credit_card),
+            Text("Payment Methods")
+          ]),
+          Row(children: const <Widget>[
+            Icon(Icons.card_giftcard_outlined),
+            Text("Promo Card")
+          ]),
+          Row(children: const <Widget>[
+            Icon(Icons.add_alert_sharp),
+            Text("Notifaction")
+          ]),
+          Row(children: const <Widget>[
+            Icon(Icons.help_outline_outlined),
+            Text("Help")
+          ]),
+          Row(children: const <Widget>[
+            Icon(Icons.align_vertical_bottom_outlined),
+            Text("About")
+          ]),
+        ],
       ),
     );
   }
